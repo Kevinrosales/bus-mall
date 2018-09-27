2b8b7cd854bc1170ad36cd0ab0026060ebbffbb6
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-09-27 08:42:39 -0700

    almost got the chart working

--- js/app.js
+++ js/app.js
@@ -6,8 +6,10 @@ var imgElement3 = document.getElementById('slot3');
 var randomNumber1
 var randomNumber2
 var randomNumber3
-var detail = document.getElementById('detail');
-var maxClicksAllowed = 25
+// var detail = document.getElementById('detail');
+var maxClicksAllowed = 25;
+var ctx = document.getElementById("myChart");
+
 
 function Item(filepath, fileName, numberOfTimesShown =0, votes =0) {
     this.imgSource = filepath;  
@@ -101,20 +103,97 @@ function eventHandler(event){
     Item.allItems[randomNumber3].numberOfTimesShown++;
 }
 
-function summarizeData() {
-    var ul = document.createElement('ul');
+// function summarizeData() {
+//     var ul = document.createElement('ul');
 
-    for(var i=0; i<Item.allItems.length; i++) {
-        var li = document.createElement('li');
-        li.textContent = `
-        ${Item.allItems[i].fileName}
-        Views: ${Item.allItems[i].numberOfTimesShown}
-        votes: ${Item.allItems[i].votes}
-        `;
-    ul.appendChild(li);
+//     for(var i=0; i<Item.allItems.length; i++) {
+//         var li = document.createElement('li');
+//         li.textContent = `
+//         ${Item.allItems[i].fileName}
+//         Views: ${Item.allItems[i].numberOfTimesShown}
+//         votes: ${Item.allItems[i].votes}
+//         `;
+//     ul.appendChild(li);
+//     }
+//     detail.appendChild(ul);
+// }
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Bag", "Banana", "Bathroom", "Boots", "Breakfast", "Bubblegum", "Chair", "Cthulhu", "Dog-Duck", "Dragon", "Pen", "Pet Sweep", "Scissors", "Shark", "Sweep", "Tauntaun", "Unicorn", "Usb", "Water Can", "Wine Glass"],
+        datasets: [{
+            label: '# of votes',
+            data: [Item.allItems[0].votes, Item.allItems[1].votes, Item.allItems[2].votes, Item.allItems[3].votes, Item.allItems[4].votes, Item.allItems[5].votes, Item.allItems[6].votes, Item.allItems[7].votes, Item.allItems[8].votes, Item.allItems[9].votes, Item.allItems[10].votes, Item.allItems[11].votes, Item.allItems[12].votes, Item.allItems[13].votes, Item.allItems[14].votes, Item.allItems[15].votes, Item.allItems[16].votes, Item.allItems[17].votes, Item.allItems[18].votes, Item.allItems[19].votes,],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)' ,
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+                
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        title:{
+            display: true,
+            text: 'Results of Survey'
+        },
+        scales: {
+            gridLines:[{
+                tickMarkLength: 50
+            }],
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    stepSize: 1, 
+                },
+                weight: -20
+            }]
+        }
+    
     }
-    detail.appendChild(ul);
-}
+});
+
 
 initializeDatabase();
 setupEventListeners();
